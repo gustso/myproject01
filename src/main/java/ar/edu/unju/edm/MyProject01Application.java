9d6ca72a26687865729b864ee93583f1bd2f043d
NTGustso <NTGustso@DESKTOP-0IA820K>
2020-05-13 17:29:35 -0700

    Asunto: Inicializacion del Proyecto Resposable: Gustavo Sebastian Sosa Task00: inicializacion del Proyecto

---
+++ src/main/java/ar/edu/unju/edm/MyProject01Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject01Application.class, args);
+	}
+
+}
